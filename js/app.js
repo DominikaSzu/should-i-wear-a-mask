c5b6bf483867a84056997ab7fd4ad333e150289a
Dominika Szukała <dominika.szukala@gmail.com>
2019-01-27 12:17:36 +0100

    Getting the localization

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
+const localization = document.querySelector('.localization');
 const myPromise = fetch('https://api.waqi.info/feed/here/?token=');
 
+function getCity(str) {
+
+}
+
 myPromise.then(data => data.json())
-		.then(data => console.log(data))
+		.then(data => {
+			return localization.textContent = data.data.city.name;
+			console.log(data.data.city.name)
+		})
+		.catch(err => console.error(err));
