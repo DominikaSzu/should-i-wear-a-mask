36fc392ef5e68ef656090748b4c7be33424244dc
Dominika Szukała <dominika.szukala@gmail.com>
2019-01-28 15:08:24 +0100

    Display info about air

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
 const localization = document.querySelector('.localization');
+const pm10Info = document.querySelector('.pm10-result');
+const pm25Info = document.querySelector('.pm25-result');
+const o3Info = document.querySelector('.o3-result');
 const myPromise = fetch('https://api.waqi.info/feed/here/?token=');
+const results = document.querySelectorAll('.result')
 
-function getCity(str) {
-
-}
-
-myPromise.then(data => data.json())
-		.then(data => {
-			return localization.textContent = data.data.city.name;
-			console.log(data.data.city.name)
+myPromise.then(response => response.json())
+		.then(response => {
+			console.log(response)
+			results.forEach(result => {
+				const info = result.dataset.name;
+				const infoValue = response.data.iaqi[info];
+				result.textContent = infoValue.v;
+			})
 		})
 		.catch(err => console.error(err));
