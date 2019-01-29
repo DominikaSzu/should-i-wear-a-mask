a0dcc0e5988502916adde99389195f59820bb276
Dominika Szukała <dominika.szukala@gmail.com>
2019-01-29 13:06:01 +0100

    Fuctions to control the level of air pollution with pm10 and pm25

--- js/app.js
+++ js/app.js
@@ -5,6 +5,39 @@ const o3Info = document.querySelector('.o3-result');
 const myPromise = fetch('https://api.waqi.info/feed/here/?token=bec1f2834d0747c3fad4a5968e913404e4b1d0df');
 const results = document.querySelectorAll('.result')
 
+function airQualityPm25(num) {
+	let pm25Level = '';
+	if (num <= 10) {
+		pm25Level = 'good';
+	} else if (num > 10 && num <= 20) {
+		pm25Level = 'fair';
+	} else if (num > 20 && num <= 25) {
+		pm25Level = 'moderate';
+	} else if (num > 25 && num <= 50) {
+		pm25Level = 'poor';
+	} else if (num > 50 && num <= 800) {
+		pm25Level = 'very poor';
+	}
+	return pm25Level;
+}
+
+function airQualityPm10(num) {
+	let pm10Level = '';
+	if (num <= 20) {
+		pm10Level = 'good';
+	} else if (num > 20 && num <= 35) {
+		pm10Level = 'fair';
+	} else if (num > 35 && num <= 50) {
+		pm10Level = 'moderate';
+	} else if (num > 50 && num <= 100) {
+		pm10Level = 'poor';
+	} else if (num > 100 && num <= 1200) {
+		pm10Level = 'very poor';
+	}
+	return pm10Level;
+}
+
+
 myPromise.then(response => response.json())
 		.then(response => {
 			console.log(response)
@@ -12,6 +45,7 @@ myPromise.then(response => response.json())
 				const info = result.dataset.name;
 				const infoValue = response.data.iaqi[info];
 				result.textContent = infoValue.v;
+
 			})
 		})
 		.catch(err => console.error(err));
