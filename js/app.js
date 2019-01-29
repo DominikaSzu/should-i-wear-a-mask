071bad73e261b45c01f8becf7f2e25093dbca148
Dominika Szukała <dominika.szukala@gmail.com>
2019-01-29 11:52:15 +0100

    h1 fixed and list added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const localization = document.querySelector('.localization');
 const pm10Info = document.querySelector('.pm10-result');
 const pm25Info = document.querySelector('.pm25-result');
 const o3Info = document.querySelector('.o3-result');
-const myPromise = fetch('https://api.waqi.info/feed/here/?token=');
+const myPromise = fetch('https://api.waqi.info/feed/here/?token=bec1f2834d0747c3fad4a5968e913404e4b1d0df');
 const results = document.querySelectorAll('.result')
 
 myPromise.then(response => response.json())
