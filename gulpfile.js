329fb450d890f4b8e3af43c5bddc7bd95a090115
Dominika Szukała <dominika.szukala@gmail.com>
2019-02-09 19:53:39 +0100

    Gulp watch sass task addeD

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-	return gulp.src('app/scss/styles.scss')
+	return gulp.src('app/scss/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/scss/*.scss', gulp.series('sass'));
 });
