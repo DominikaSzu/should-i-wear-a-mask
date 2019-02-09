1c02d0ccd7544826538dfae71173d1016c2d52d9
Dominika Szukała <dominika.szukala@gmail.com>
2019-02-09 19:37:19 +0100

    Gulp added to project with sass task

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+	return gulp.src('app/scss/styles.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+});
